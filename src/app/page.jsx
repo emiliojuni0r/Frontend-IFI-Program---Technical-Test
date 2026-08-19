d8abe57108eb564d6c67f75d9f1ec78463d2cbc4
emiliojuni0r <emiliohamsyahjr@gmail.com>
2026-08-19 20:52:19 +0700

    feat : add homepage

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import Banner from "@/components/homepage/banner";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main >
+      <Banner />
+      <div className="container mx-auto relative z-20 h-full flex flex-col justify-between pt-30 pb-20 md:pt-60 md:pb-40">
+
+    </div>
+    </main >
+
+  );
+}
